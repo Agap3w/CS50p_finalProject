242495da93c56945eef32294c6e8a1e24241d6e4
Agap3w <thefalse110@gmail.com>
2024-11-19 15:54:17 +0100

    inizio progetto personale slegato da tutorial

--- CS50P-Final_Project/snake_game.py
+++ CS50P-Final_Project/snake_game.py
@@ -1,31 +1,32 @@
-#creo e aggiungo score, sound effect
-
 import pygame, sys, random
 from pygame.math import Vector2 
 
+#il serpente
 class SNAKE: 
     def __init__(self):
         self.body = [Vector2(5,10), Vector2(4,10), Vector2(3,10)] 
-        self.direction = Vector2(1,0)
+        self.direction = Vector2(0,0)
         self.new_block = False
 
-        self.head_up = pygame.image.load('../static/head_up.png').convert_alpha()
-        self.head_down = pygame.image.load('../static/head_down.png').convert_alpha()
-        self.head_right = pygame.image.load('../static/head_right.png').convert_alpha()
-        self.head_left = pygame.image.load('../static/head_left.png').convert_alpha()
+        self.head_up = pygame.image.load('static/head_up.png').convert_alpha()
+        self.head_down = pygame.image.load('static/head_down.png').convert_alpha()
+        self.head_right = pygame.image.load('static/head_right.png').convert_alpha()
+        self.head_left = pygame.image.load('static/head_left.png').convert_alpha()
 		
-        self.tail_up = pygame.image.load('../static/tail_up.png').convert_alpha()
-        self.tail_down = pygame.image.load('../static/tail_down.png').convert_alpha()
-        self.tail_right = pygame.image.load('../static/tail_right.png').convert_alpha()
-        self.tail_left = pygame.image.load('../static/tail_left.png').convert_alpha()
+        self.tail_up = pygame.image.load('static/tail_up.png').convert_alpha()
+        self.tail_down = pygame.image.load('static/tail_down.png').convert_alpha()
+        self.tail_right = pygame.image.load('static/tail_right.png').convert_alpha()
+        self.tail_left = pygame.image.load('static/tail_left.png').convert_alpha()
 
-        self.body_vertical = pygame.image.load('../static/body_vertical.png').convert_alpha()
-        self.body_horizontal = pygame.image.load('../static/body_horizontal.png').convert_alpha()
+        self.body_vertical = pygame.image.load('static/body_vertical.png').convert_alpha()
+        self.body_horizontal = pygame.image.load('static/body_horizontal.png').convert_alpha()
 
-        self.body_tr = pygame.image.load('../static/body_tr.png').convert_alpha()
-        self.body_tl = pygame.image.load('../static/body_tl.png').convert_alpha()
-        self.body_br = pygame.image.load('../static/body_br.png').convert_alpha()
-        self.body_bl = pygame.image.load('../static/body_bl.png').convert_alpha()
+        self.body_tr = pygame.image.load('static/body_tr.png').convert_alpha()
+        self.body_tl = pygame.image.load('static/body_tl.png').convert_alpha()
+        self.body_br = pygame.image.load('static/body_br.png').convert_alpha()
+        self.body_bl = pygame.image.load('static/body_bl.png').convert_alpha()
+
+        self.crunch_sound = pygame.mixer.Sound("static/crunch.wav") #uploado un sound
 
     def draw_snake(self):
 
@@ -94,6 +95,14 @@ class SNAKE:
     def add_block(self):
         self.new_block = True
 
+    def play_crunch_sound(self):
+        self.crunch_sound.play()
+
+    def reset(self):
+        self.body = [Vector2(5,10), Vector2(4,10), Vector2(3,10)]
+        self.direction = Vector2(0,0)
+
+#la frutta
 class FRUIT:
     def __init__(self):
         self.x = random.randint(0,cell_number-1)
@@ -103,13 +112,14 @@ class FRUIT:
     
     def draw_fruit(self):
         fruit_rect = pygame.Rect(self.pos.x*cell_size, self.pos.y*cell_size, cell_size, cell_size)
-        screen.blit(scaled_legna, fruit_rect)
+        screen.blit(apple, fruit_rect)
 
     def randomize(self):
         self.x = random.randint(0,cell_number-1)
         self.y = random.randint(0,cell_number-1)
         self.pos = Vector2(self.x, self.y)
-        
+
+#MAIN (include serpente + frutta + background + dinamiche di gioco come collisione e game_over)        
 class MAIN:
     def __init__(self):
         self.snake = SNAKE()
@@ -130,6 +140,12 @@ class MAIN:
         if self.fruit.pos == self.snake.body[0]: 
             self.fruit.randomize()
             self.snake.add_block()
+            self.snake.play_crunch_sound()
+        
+        #se frutta respawna su serpente, ripeto respawn
+        for block in self.snake.body[1:]:
+            if block == self.fruit.pos:
+                self.fruit.randomize()
 
     def check_fail(self):
         if not 0 <= self.snake.body[0].x < cell_number or not 0 <= self.snake.body[0].y < cell_number:
@@ -140,25 +156,24 @@ class MAIN:
                 self.game_over()
 
     def game_over(self):
-        pygame.quit()
-        sys.exit(1)
+        self.snake.reset()
 
     def draw_grass(self):
         
-        grass_colour = (10,110,110)
+        grass_colour = (135,170,60)
 
         for row in range(cell_number):
             if row % 2 == 0:
                 for col in range(cell_number):
                     if col % 2 == 0:
                         grass_rect = pygame.Rect(col*cell_size, row*cell_size, cell_size, cell_size)
-                        pygame.draw.rect(screen, grass_colour, grass_rect)         
+                        pygame.draw.rect(screen, grass_colour, grass_rect)       
             else:
                 for col in range(cell_number):
                     if col % 2 != 0:
                         grass_rect = pygame.Rect(col*cell_size, row*cell_size, cell_size, cell_size) 
                         pygame.draw.rect(screen, grass_colour, grass_rect)
-    
+
     # creo e disegno score (il font l'ho creato sotto invece, fuori dalle classi)
     def draw_score(self):
         score_text = str(len(self.snake.body) - 3) # converto int in str perché è più semplice poi da printare/gestire (boh!)
@@ -168,14 +183,15 @@ class MAIN:
         score_x = cell_size * cell_number - 60
         score_y = cell_size * cell_number - 40
         score_rect = score_surface.get_rect(center = (score_x, score_y)) # creo il rettangolo dalla score_surface
-        legna_rect = scaled_legna.get_rect(midright = (score_rect.left-10, score_rect.centery)) # creo il rettangolo dalla scaled_legna(surface). midright mi permette di piazzarmi direttamente sul rettangolo adiacente
-        score_frame_rect = pygame.Rect(legna_rect.left, legna_rect.top,cell_size+score_rect.width+20, cell_size)
+        apple_rect = apple.get_rect(midright = (score_rect.left-10, score_rect.centery)) # creo il rettangolo dalla apple(surface). midright mi permette di piazzarmi direttamente sul rettangolo adiacente
+        score_frame_rect = pygame.Rect(apple_rect.left, apple_rect.top,cell_size+score_rect.width+20, cell_size)
 
         #posiziono i quattro elementi che compongono lo score:
         pygame.draw.rect(screen, (167,209,61), score_frame_rect) # il rettangolino di sfondo
         screen.blit(score_surface, score_rect) # il numero
-        screen.blit(scaled_legna, legna_rect) # la legna
+        screen.blit(apple, apple_rect) # la mela
         pygame.draw.rect(screen, (110,110,110), score_frame_rect, 2) # la cornice
+
 
 
 pygame.init()
@@ -185,8 +201,7 @@ cell_number = 20
 
 screen = pygame.display.set_mode((cell_number*cell_size, cell_number*cell_size)) 
 clock = pygame.time.Clock()
-legna = pygame.image.load("../static/legna.png").convert_alpha()
-scaled_legna = pygame.transform.scale(legna, (cell_size, cell_size))
+apple = pygame.image.load("static/apple.png").convert_alpha()
 game_font = pygame.font.Font(None, 25) #ARG = (font, fontsize). Come font posso usare 'None' oppure uploadare un '../static/font.ttf'
 
 SCREEN_UPDATE = pygame.USEREVENT
@@ -213,7 +228,7 @@ while True:
             if event.key == pygame.K_RIGHT and main_game.snake.direction.x != -1: 
                 main_game.snake.direction = Vector2(1,0)
 
-    screen.fill((0,100,100)) 
+    screen.fill((145,182,63)) 
     main_game.draw_elements()
     pygame.display.update()
     clock.tick(60)
